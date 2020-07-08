d2b7a0271b92a8b199be14f65e1c495700aa321e
James Tam <james.kit.tam@gmail.com>
2020-07-08 14:37:38 -0700

    Change message text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "EKS WORKSHOP CHANGE TEST"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
